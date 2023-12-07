29d8c51daf2812d258303deac44698cf7064c2df
cyoger <akshat1sharma2@gmail.com>
2023-12-06 21:44:50 -0500

    very late :((

---
+++ Day_3/day_3_part_1.py
@@ -0,0 +1,33 @@
+import re
+from collections import defaultdict
+from math import prod
+
+def part_sum_gears(lines):
+
+
+    symbols = dict()
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if c not in "1234567890.":
+                symbols[(x, y)] = c
+
+    gears = defaultdict(list)
+    part_numbers_sum = 0
+    for y, line in enumerate(lines):
+        for match in re.finditer(r"\d+", line):
+            for (s_x, s_y), c in symbols.items():
+                if (match.start() - 1 <= s_x <= match.end()) and (y - 1 <= s_y <= y + 1):
+                    num = int(match.group())
+                    part_numbers_sum += num
+                    if c == "*":
+                        gears[(s_x, s_y)].append(num)
+                    break
+                
+    return part_numbers_sum
+
+
+with open("input.txt") as r:
+    lines = r.read().split("\n")
+
+part_sum = part_sum_gears(lines)
+print(part_sum)
